e5c77318003ed304b9b2cedcb56b1b52123855d4
emanuel-descomplicar <emanuel@descomplicar.pt>
2025-03-10 09:33:52 +0000

    Cria estrutura inicial de diretórios

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+"""
+GeradorWP - Gerador automático de artigos para WordPress
+
+Autor: Descomplicar - Agência de Aceleração Digital
+https://descomplicar.pt
+"""
+
+__version__ = '1.0.0'
+__author__ = 'Descomplicar - Agência de Aceleração Digital'
+__email__ = 'geral@descomplicar.pt'
